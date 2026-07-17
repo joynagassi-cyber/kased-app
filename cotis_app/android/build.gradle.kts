8d04bdfac161967c850e625a4a2917389055b85f
JOY-py-arch <joynagassi@gmail.com>
2026-07-17 17:14:27 +0100

    fix(ci): remove compileSdk patch from build.gradle.kts, rely on workflow sed patch only

--- cotis_app/android/build.gradle.kts
+++ cotis_app/android/build.gradle.kts
@@ -29,22 +29,6 @@ subprojects {
 
 subprojects {
     project.evaluationDependsOn(":app")
-
-    plugins.withId("com.android.library") {
-        afterEvaluate {
-            extensions.findByType(com.android.build.gradle.LibraryExtension::class.java)?.let { libExt ->
-                // Set namespace if missing
-                if (libExt.namespace.isNullOrEmpty()) {
-                    libExt.namespace = "io.flutter.plugins.${this@subprojects.name}"
-                }
-                // Set compileSdk 31 if lower (fixes android:attr/lStar in isar_flutter_libs)
-                val currentCompileSdk = libExt.compileSdkVersion?.toString()?.removePrefix("android-")?.toIntOrNull() ?: 0
-                if (currentCompileSdk < 31) {
-                    libExt.compileSdkVersion(31)
-                }
-            }
-        }
-    }
 }
 
 tasks.register<Delete>("clean") {
