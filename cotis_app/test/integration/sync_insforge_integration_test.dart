622b6ea87935e503f0e562f2bc3bdf36bf048142
JOY-py-arch <joynagassi@gmail.com>
2026-08-15 00:28:56 +0100

    fix(test): remove unused imports in integration tests

--- cotis_app/test/integration/sync_insforge_integration_test.dart
+++ cotis_app/test/integration/sync_insforge_integration_test.dart
@@ -1,11 +1,6 @@
-import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kased_app/core/insforge/insforge_service.dart';
-import 'package:kased_app/core/insforge/insforge_config.dart';
-import 'package:kased_app/models/cotisation.dart';
-import 'package:kased_app/models/culte.dart';
-import 'package:kased_app/models/membre.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockDio extends Mock implements Dio {}
